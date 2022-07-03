bf746dbd2e36daec012f6e4d1dde3649ca1f6a82
Alexander <sanya55bychyk@gmail.com>
2022-07-03 22:04:05 +0300

    feat: add modal

--- surprise-me-lib/demo.js
+++ surprise-me-lib/demo.js
@@ -6,6 +6,13 @@ function Widget(config) {
     }
   }
 
+  function appendStyles() {
+    let stylesheet = document.createElement('link');
+    stylesheet.setAttribute('href', `http://localhost:3355/modal.css`)
+    stylesheet.setAttribute('rel', 'stylesheet');
+    document.head.append(stylesheet);
+  }
+  
   function createButton({placeholder_text, placeholder_class = 'placeholder-button', placeholder_wrapper_class = 'placeholder-wrapper'}) {
     let wrapper = document.createElement('div');
     let button = document.createElement('button');
@@ -14,16 +21,33 @@ function Widget(config) {
     button.className = placeholder_class;
     button.textContent = placeholder_text;
 
-    button.addEventListener('click', () => alert('opened'));
+    button.addEventListener('click', openModal);
 
     wrapper.append(button);
     return wrapper;
   }
 
-  function appendStyles() {
-    let stylesheet = document.createElement('link');
-    stylesheet.setAttribute('href', `http://localhost:3355/modal.css`)
-    stylesheet.setAttribute('rel', 'stylesheet');
-    document.head.append(stylesheet);
+  function openModal() {
+    document.body.append(createModal());
+  }
+
+  function createModal() {
+    let modal = document.createElement('div');
+    let window = document.createElement('div');
+
+    modal.className = 'modal';
+    window.className = 'modal-window';
+
+    modal.addEventListener('click', () => closeModal(modal));
+    window.addEventListener('click', (e) => {
+      e.stopPropagation()
+    });
+
+    modal.append(window);
+    return modal;
+  }
+
+  function closeModal(modal) {
+    modal.remove();
   }
 }
